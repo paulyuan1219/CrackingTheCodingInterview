fac3e6e5b358078ebf4d4fc3e831245fb0528086
paulyuan1219 <paulyuan1219@gmail.com>
2017-02-20 22:55:53 -0500

    Update main.py
    Add func_2_1

--- ch01/main.py
+++ ch01/main.py
@@ -1,5 +1,22 @@
 import sys
 
+def func_2_1(str1, str2):
+    s1 = str1.sort()
+    s2 = str2.sort()
+    
+    if s1==s2:
+        return True
+    else:
+        return False
+def call_func_2_1():
+    while(True):
+        str = raw_input("Please input 2 strings:(No space allowed within one string)\n")
+        fields = str.split()
+        
+        result = func_2_1(fields[0], fields[1])
+        print(result)
+        print("\n")
+
 def func_1_1(str):
     """
     check for unique chars
@@ -42,9 +59,10 @@ def call_func_1_2():
         print(result)
         print("\n")
 
+                       
 
 def main():
-    call_func_1_2()
+    call_func_2_1()
 
 if __name__ == "__main__":
     main()
